3757f5ba50c13752d0a843bc2850d4b41e3212d2
cviogomes <123747448+cviogomes@users.noreply.github.com>
2025-05-06 09:36:18 -0300

    Create Personagem.hpp

---
+++ Personagem.hpp
@@ -0,0 +1,32 @@
+#ifndef PERSONAGEM_HPP
+#define PERSONAGEM_HPP
+#include <string>
+
+class Personagem {
+public:
+    Personagem(const std::string& nome, int vidaMax, int ataque, int defesa);
+    virtual ~Personagem() = default;
+    virtual void atacar(Personagem& alvo);
+    virtual void defender();
+    void restaurarVida();
+
+    bool estaVivo() const;
+
+    const std::string& getNome() const;
+    int getVida() const;
+    int getVidaMax() const;
+
+
+
+protected:
+    std::string nome_;
+    int vida_;
+    int vidaMax_;
+    int ataque_;
+    int defesa_;
+    
+};
+
+
+
+#endif // PERSONAGEM_HPP
