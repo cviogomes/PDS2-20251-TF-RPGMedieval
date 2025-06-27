729d842c16a7e81a662f5440ea550a539011837f
joshuadcr <167485208+joshuadcr@users.noreply.github.com>
2025-06-27 15:47:33 -0300

    Update CavaleiroDaPeste.hpp
    Atualiza o inimigo Cavaleiro da Peste

--- include/CavaleiroDaPeste.hpp
+++ include/CavaleiroDaPeste.hpp
@@ -2,25 +2,24 @@
 #define CAVALEIRO_DA_PESTE_HPP
 
 #include "Inimigo.hpp"
+#include "Jogador.hpp"
+#include "Utils.hpp"
 #include <algorithm>
 #include <iostream>
-#include <vector>
 #include <memory>
 
 class CavaleiroDaPeste : public Inimigo {
-    public:
-    CavaleiroDaPeste() : Inimigo("Cavaleiro da Peste", 200, 30, 10) {}
-    
+public:
+    CavaleiroDaPeste() : Inimigo("Seraphina, Cavaleira da Peste", 200, 30, 10) {}
+
     void aoAtacar(Jogador& jogador) override {
         int reducao = 1;
         jogador.setDefesa(std::max(0, jogador.getDefesa() - reducao));
-        std::cout << nome_ << " contamina você com a peste! Defesa agora em " << jogador.getDefesa() << "\n";
+        battlePrint(nome_ + " contamina você com a peste! Sua defesa foi reduzida para " + std::to_string(jogador.getDefesa()) + "!\n");
     }
 
     std::unique_ptr<Inimigo> clone() const override {
-    return std::make_unique<CavaleiroDaPeste>(*this);
+        return std::make_unique<CavaleiroDaPeste>(*this);
     }
-
 };
-
 #endif
