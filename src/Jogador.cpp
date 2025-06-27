c1b8bba432d3fa5784f4d9c5bc3dc35184ba439d
joshuadcr <167485208+joshuadcr@users.noreply.github.com>
2025-06-27 16:14:46 -0300

    Atualização do Sistema da Loja
    Expande e refatora o sistema da Loja

--- src/Jogador.cpp
+++ src/Jogador.cpp
@@ -71,7 +71,79 @@ bool Jogador::temAliadoNyx() const {
     return aliadoNyx_;
 }
 
-void Jogador::visitarLoja() {
+void Jogador::visitarLoja(int capitulo) {
+    typeText("\n--- Loja do Mercador Errante ---\n", TextSpeed::NORMAL);
+    retornarInvestimento();
+
+    int escolha = 0;
+    do {
+        typeText("\nOuro atual: " + std::to_string(ouro_) + "\n", TextSpeed::NORMAL);
+        typeText("---------------------------------\n", TextSpeed::NORMAL);
+
+        if (capitulo == 1) {
+            typeText("1 - Melhorar Ataque (+5)  (50 Ouro)\n", TextSpeed::NORMAL);
+            typeText("2 - Melhorar Defesa (+5)  (50 Ouro)\n", TextSpeed::NORMAL);
+            typeText("3 - Comprar Poção de Cura (20 Ouro)\n", TextSpeed::NORMAL);
+            typeText("4 - Investir Ouro (Retorno de 50%)\n", TextSpeed::NORMAL);
+        } else if (capitulo == 2) {
+            typeText("1 - Melhorar Ataque (+5)      (50 Ouro)\n", TextSpeed::NORMAL);
+            typeText("2 - Melhorar Vida Máxima (+10) (60 Ouro)\n", TextSpeed::NORMAL);
+            typeText("3 - Comprar Poção de Cura      (40 Ouro)\n", TextSpeed::NORMAL);
+            typeText("4 - Investir Ouro (Retorno de 50%)\n", TextSpeed::NORMAL);
+        } else if (capitulo == 3) {
+            typeText("1 - Melhorar Defesa (+5)       (60 Ouro)\n", TextSpeed::NORMAL);
+            typeText("2 - Melhorar Vida Máxima (+15)  (80 Ouro)\n", TextSpeed::NORMAL);
+            typeText("3 - Comprar Poção de Cura       (60 Ouro)\n", TextSpeed::NORMAL);
+            typeText("4 - Investir Ouro (Retorno de 50%)\n", TextSpeed::NORMAL);
+        }
+        typeText("5 - Sair da loja\n", TextSpeed::NORMAL);
+
+        std::cout << "Opção: ";
+        std::cin >> escolha;
+
+        switch (escolha) {
+            case 1:
+                if(capitulo == 1 || capitulo == 2) {
+                    if (ouro_ >= 50) { ataque_ += 5; ouro_ -= 50; typeText("Ataque aumentado!\n", TextSpeed::NORMAL); }
+                    else { typeText("Ouro insuficiente!\n", TextSpeed::NORMAL); }
+                } else if (capitulo == 3) {
+                     if (ouro_ >= 60) { defesa_ += 5; ouro_ -= 60; typeText("Defesa aumentada!\n", TextSpeed::NORMAL); }
+                     else { typeText("Ouro insuficiente!\n", TextSpeed::NORMAL); }
+                }
+                break;
+            case 2:
+                if (capitulo == 1) {
+                     if (ouro_ >= 50) { defesa_ += 5; ouro_ -= 50; typeText("Defesa aumentada!\n", TextSpeed::NORMAL); }
+                     else { typeText("Ouro insuficiente!\n", TextSpeed::NORMAL); }
+                } else if (capitulo == 2 || capitulo == 3) {
+                    int custo = (capitulo == 2) ? 60 : 80;
+                    int bonus = (capitulo == 2) ? 10 : 15;
+                    if (ouro_ >= custo) { vidaMax_ += bonus; vida_ = vidaMax_; ouro_ -= custo; typeText("Vida máxima aumentada!\n", TextSpeed::NORMAL); }
+                    else { typeText("Ouro insuficiente!\n", TextSpeed::NORMAL); }
+                }
+                break;
+            case 3: {
+                int custo = (capitulo == 1) ? 20 : (capitulo == 2 ? 40 : 60);
+                if (ouro_ >= custo) { pocoes_++; ouro_ -= custo; typeText("Você comprou uma poção!\n", TextSpeed::NORMAL); }
+                else { typeText("Ouro insuficiente!\n", TextSpeed::NORMAL); }
+                break;
+            }
+            case 4: {
+                int valor;
+                typeText("Quanto deseja investir? ", TextSpeed::NORMAL);
+                std::cin >> valor;
+                investirOuro(valor);
+                break;
+            }
+            case 5:
+                typeText("Você saiu da loja.\n", TextSpeed::NORMAL);
+                break;
+            default:
+                typeText("Opção inválida.\n", TextSpeed::NORMAL);
+                break;
+        }
+    } while (escolha != 5);
+}
 
     std::cout << "\n--- Loja ---\n";
     retornarInvestimento();
