60db1fca52805c76f4ce5711a65f64148004e230
joshuadcr <167485208+joshuadcr@users.noreply.github.com>
2025-06-27 15:48:12 -0300

    Update CavaleiroDaFome.hpp
    Atualiza o inimigo Cavaleiro da Fome

--- include/CavaleiroDaFome.hpp
+++ include/CavaleiroDaFome.hpp
@@ -1,34 +1,25 @@
 #ifndef CAVALEIRO_DA_FOME_HPP
-#define CAVALEIRO_DA_FOM_HPP
+#define CAVALEIRO_DA_FOME_HPP
 
 #include "Inimigo.hpp"
+#include "Jogador.hpp"
+#include "Utils.hpp"
 #include <iostream>
-#include <vector>
 #include <memory>
 
-class CavaleiroDaFome : public Inimigo{
-    public:
-    CavaleiroDaFome() : Inimigo("Cavaleiro da Fome", 400, 20, 8){}
+class CavaleiroDaFome : public Inimigo {
+public:
+    CavaleiroDaFome() : Inimigo("Vorstag, Cavaleiro da Fome", 400, 20, 8) {}
 
-    int getVidaMax() const {
-    return vidaMax_;
-    }
-
-    void setVidaMax(int novaVidaMax) {
-    vidaMax_ = novaVidaMax;
-    }
-    
     void aoAtacar(Jogador& jogador) override {
         jogador.setVidaMax(std::max(10, jogador.getVidaMax() - 5));
         if (jogador.getVida() > jogador.getVidaMax())
             jogador.setVida(jogador.getVidaMax());
-        std::cout << nome_ << " te enfraquece! Vida máxima agora em " << jogador.getVidaMax() << "\n";
+        battlePrint(nome_ + " te enfraquece com a fome! Sua vida máxima agora é " + std::to_string(jogador.getVidaMax()) + "!\n");
     }
 
     std::unique_ptr<Inimigo> clone() const override {
-    return std::make_unique<CavaleiroDaFome>(*this);
+        return std::make_unique<CavaleiroDaFome>(*this);
     }
-
 };
-
 #endif
