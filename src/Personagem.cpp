fef06f983b603d83a2470710443aac54866f3ca6
stanciola <vitorstanciola8@gmail.com>
2025-06-28 07:58:36 -0300

    Update Personagem.cpp
    Parametros do personagem

--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -1,56 +1,5 @@
-#include "personagem.hpp"
+PERSONAGEM.CPP ATUALIZADO
+#include "Personagem.hpp"
+#include "Utils.hpp" // Incluído para usar battlePrint
 #include <iostream>
 #include <algorithm>
-
-Personagem::Personagem(const std::string& nome, int vidaMax, int ataque, int defesa)
-    : nome_(nome),
-      vida_(vidaMax),
-      vidaMax_(vidaMax),
-      ataque_(ataque),
-      defesa_(defesa)
-{
-
-}
-
-void Personagem::atacar(Personagem& alvo){
-    //interessante comentar essa linha abaixo durante a apresentacao hein
-    int dano = std::max(1, ataque_ - alvo.defesa_);
-    alvo.vida_ -= dano;
-    std::cout << nome_ << " ataca " << alvo.nome_ << " causando " << dano << " de dano! ( " << alvo.vida_ << "/" << alvo.vidaMax_ << " )\n";
-}
-
-void Personagem::defender(){
-    //so avisa que ta defendendo
-    std::cout << nome_ << " assume postura defensiva" << std::endl;
-}
-
-void Personagem::restaurarVida(){
-    vida_ = vidaMax_;
-    std::cout << nome_ << " restaurou a vida para " << vida_ << "/" << vidaMax_ << std::endl;
-}
-
-bool Personagem::estaVivo() const {
-    return vida_ > 0;
-}
-
-const std::string& Personagem::getNome() const {
-    return nome_;
-}
-
-int Personagem::getVida() const {
-    return vida_;
-}
-
-int Personagem::getVidaMax() const {
-    return vidaMax_;
-}
-int Personagem::getAtaque() const {
-    return ataque_;
-}
-
-int Personagem::getDefesa() const {
-    return defesa_;
-}
-
-
-
