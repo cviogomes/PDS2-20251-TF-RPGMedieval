10dc15a1d322bf885c7b20fb1d6c8ecfbcc3e325
stanciola <vitorstanciola8@gmail.com>
2025-06-28 08:00:43 -0300

    Update Personagem.cpp
    refino do ataque

--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -15,3 +15,7 @@ Personagem::Personagem(const std::string& nome, int vidaMax, int ataque, int def
 void Personagem::atacar(Personagem& alvo) {
     int dano = std::max(1, ataque_ - alvo.getDefesa());
     alvo.setVida(alvo.getVida() - dano);
+
+    std::string mensagem = nome_ + " ataca " + alvo.getNome() + " causando " + std::to_string(dano) + " de dano! (" + std::to_string(alvo.getVida()) + "/" + std::to_string(alvo.getVidaMax()) + ")\n";
+    battlePrint(mensagem);
+}
