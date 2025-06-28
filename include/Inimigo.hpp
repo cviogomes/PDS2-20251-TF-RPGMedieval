e1e41b251f414a8f089bbba8dd43902291278e6b
stanciola <vitorstanciola8@gmail.com>
2025-06-27 22:12:43 -0300

    Update Inimigo.hpp
    Refino classe jogador

--- include/Inimigo.hpp
+++ include/Inimigo.hpp
@@ -11,3 +11,6 @@ public:
     Inimigo(const std::string& nome, int vida, int ataque, int defesa)
         : Personagem(nome, vida, ataque, defesa) {}
     virtual ~Inimigo() = default;
+
+    virtual void aoReceberDano(int danoRecebido) {}
+    virtual void aoAtacar(Jogador& jogador) {}
