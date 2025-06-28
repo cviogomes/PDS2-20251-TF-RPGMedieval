d7a9324096be252900cd76dddeeb3b7d8574405a
stanciola <vitorstanciola8@gmail.com>
2025-06-28 08:00:10 -0300

    Update Personagem.cpp
    personagem ataque

--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -11,3 +11,7 @@ Personagem::Personagem(const std::string& nome, int vidaMax, int ataque, int def
       ataque_(ataque),
       defesa_(defesa)
 {}
+
+void Personagem::atacar(Personagem& alvo) {
+    int dano = std::max(1, ataque_ - alvo.getDefesa());
+    alvo.setVida(alvo.getVida() - dano);
